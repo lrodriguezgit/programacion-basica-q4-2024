6d0d64d334e40e491032aaddf781ac78379e2346
pnouel <petronouel@gmail.com>
2024-10-05 18:46:32 -0300

    El ejercicio que no se habia comiteado

--- src/ejercicios/petro/ejercicio_5.java
+++ src/ejercicios/petro/ejercicio_5.java
@@ -1,5 +1,5 @@
 package ejercicios.petro;
-
+//https://www.codewars.com/kata/5641a03210e973055a00000d/train/java
 public class ejercicio_5 {
   public static void main(String[] args) {
     TwoDecimalPlaces(1.232123);
