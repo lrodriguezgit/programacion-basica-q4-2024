4299ac97ae1f24fafff0c65adec2e032a6e2d24b
pnouel <petronouel@gmail.com>
2024-10-29 15:30:08 -0300

    stuff

--- src/ejercicios/petro/clase4/CountPositivesSumNegatives.java
+++ src/ejercicios/petro/clase4/CountPositivesSumNegatives.java
@@ -2,6 +2,7 @@ package ejercicios.petro.clase4;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class CountPositivesSumNegatives {
   public static void main(String[] args) {
